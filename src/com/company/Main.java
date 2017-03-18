127ef3b589ab8f5b5b13e115dd08cc66ceb297e9
tscheys <tim.scheys@gmail.com>
2017-03-18 14:20:14 +0100

    implement polymorphism on figure superclass and rectangle, triangle subclass

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,89 +1,61 @@
 package com.company;
 
-class Box {
-    private double width;
-    private double length;
-    private double depth;
+class Figure {
+    protected double dimA;
+    protected double dimB;
 
-    Box() {
-        width = -1;
-        length = -1;
-        depth = -1;
+    Figure(double a, double b) {
+        dimA = a;
+        dimB = b;
     }
 
-    Box(double w, double l, double d) {
-        width = w;
-        length = l;
-        depth = d;
-    }
-
-    Box(double len) {
-        width = length = depth = len;
-    }
-
-    Box(Box ob) {
-        width = ob.width;
-        length = ob.length;
-        depth = ob.depth;
-    }
-
-    double volume() {
-        return width * length * depth;
-    }
-
- }
-
- class Boxweight extends Box {
-    private double weight;
-
-    Boxweight(Boxweight ob) {
-        // make copy of Boxweight object
-        super(ob);
-        weight = ob.weight;
-
-    }
-
-    Boxweight(double w, double l, double d, double m) {
-        super(w, l, d);
-        weight = m;
-    }
-
-    Boxweight() {
-        super();
-    }
- }
-
-class BoxColor extends Box{
-    private String color;
-
-    BoxColor() {
-        super();
-        color = "transparent";
-    }
-
-    BoxColor(String col) {
-        color = col;
-    }
-
-}
-
-class BoxTexture extends BoxColor{
-    private String texture;
-
-    BoxTexture() {
-        super();
-        texture = "default";
-
+    double area() {
+        return 0;
     }
 }
+
+class Triangle extends Figure {
+
+    Triangle(double a, double b) {
+        super(a, b);
+
+    }
+
+    double area() {
+        return (dimA * dimB) / 2;
+    }
+}
+
+class Rectangle extends Figure {
+
+    Rectangle(double a, double b) {
+        super(a, b);
+
+    }
+
+    double area() {
+        return dimA * dimB;
+    }
+}
+
+
 
 public class Main {
 
     public static void main(String[] args) {
-        Box b1 = new Box();
-        Boxweight b2 = new Boxweight();
-        //b2 = b1;
-        // b1 = b2;
+        Figure f = new Figure(3, 4);
+        Rectangle r = new Rectangle(2,8);
+        Triangle t = new Triangle(2, 1);
+
+        System.out.println("Area of figure is " + f.area());
+        System.out.println("Area of rectangle is" + r.area());
+
+        f = r;
+        System.out.println("area method called on figure which references rectangle subclass " + f.area());
+
+        f = t;
+        System.out.println("area method called on figure which references trianglesubclass " + f.area());
+
 
 
     }
