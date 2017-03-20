d3be2fa7789ebbfd3ec60ddb5bdb1b6e80664776
Tim Scheys <tscheys@Tims-MacBook-Pro.local>
2017-03-20 11:28:18 +0100

    add factorial recursively and memoized

--- src/com/company/Factorial.java
+++ src/com/company/Factorial.java
@@ -3,5 +3,23 @@ package com.company;
 /**
  * Created by tscheys on 18/03/2017.
  */
+import java.util.HashMap;
+
 public class Factorial {
+    // implement memoized version of factorial
+    private HashMap<Integer, Long> lookup = new HashMap();
+
+    long factorial(int n) {
+        if(n == 0) {
+            return 1;
+        } else if(lookup.containsKey(n)) {
+            return lookup.get(n);
+        } else {
+            long f = factorial(n - 1) * n;
+            lookup.put(n, f);
+            return f;
+        }
+    }
 }
+
+
