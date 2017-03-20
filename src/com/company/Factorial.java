f81bb35bc98d07af89682e09d1f7a545036f4418
tscheys <tim.scheys@gmail.com>
2017-03-20 11:18:50 +0100

    Implement simple factorial recursively

--- src/com/company/Factorial.java
+++ src/com/company/Factorial.java
@@ -1,7 +1,24 @@
 package com.company;
 
+import java.util.HashMap;
+
 /**
  * Created by tscheys on 18/03/2017.
  */
 public class Factorial {
+    // implement memoized version of factorial
+    private HashMap<Integer, Long> lookup = new HashMap();
+
+    long factorial(int n) {
+        if(n == 0) {
+            return 1;
+        } else if(lookup.containsKey(n)) {
+            return lookup.get(n);
+        } else {
+            long f = factorial(n - 1) * n;
+            lookup.put(n, f);
+            return f;
+        }
+    }
 }
+
