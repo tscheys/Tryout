bdf4d79e053687f73da897edcf0a0d8a2d084a68
Tim Scheys <tscheys@Tims-MacBook-Pro.local>
2017-03-20 11:23:13 +0100

    add simple factorial recursively

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,21 +19,40 @@ public class Main {
 
 
         // check functioning of area method
-        System.out.println("Area of figure is " + f.area());
-        System.out.println("Area of rectangle is" + r.area());
+        System.out.println("Area or surface of figure is " + f.area());
+        System.out.println("Area or surface of rectangle is" + r.area());
 
         // check polymorphism
         f = r;
-        System.out.println("area method called on figure which references rectangle subclass " + f.area());
+        System.out.println("area or surface method called on figure which references rectangle subclass " + f.area());
 
         f = t;
-        System.out.println("area method called on figure which references trianglesubclass " + f.area());
+        System.out.println("area or surface method called on figure which references trianglesubclass " + f.area());
 
 
         Figure f2;
 
         f2 = r;
         f2.area();
+
+        Factorial memo = new Factorial();
+        Factorial2 nonmemo = new Factorial2();
+
+/*        long startTime = System.currentTimeMillis();
+        long result1 = memo.factorial(40);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Total execution time of memoized version first run: " + (endTime-startTime) + "ms" + " result: " + result1);
+
+        long startTime3 = System.currentTimeMillis();*/
+//        long result3 = memo.factorial(40);
+//        long endTime3 = System.currentTimeMillis();
+//        System.out.println("Total execution time of memoized version second run: " + (endTime3-startTime3) + "ms" + " result: " + result3);
+
+
+/*        long startTime1 = System.currentTimeMillis();
+        long result2 = nonmemo.factorial(40);*/
+        /**/long endTime1 = System.currentTimeMillis();
+//        System.out.println("Total execution time of non-memoized version: " + (endTime1-startTime1) + "ms" + " result: " + result2);
 
 
 
